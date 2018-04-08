4ccd4c8281326ca86810b2f34027664f5cf494bb
Rohan Gupta <rohangupta@Rohans-MacBook-Pro.local>
2018-04-08 15:26:04 -0700

    incorporated a deal function

--- Card-Game/cards/deck.go
+++ Card-Game/cards/deck.go
@@ -21,7 +21,8 @@ func newDeck() deck {
 	return cards
 }
 
-func deal(d deck, handSize int) {
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 
 }
 func (d deck) print() {
